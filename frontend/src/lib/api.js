6a22cb973c41378bf8d24288fcf88fd0ad568ee3
Nika Abrumia <154442798+bassNEXTRO712@users.noreply.github.com>
2026-07-23 14:25:11 +0200

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://თქვენი-ბექენდის-ლინკი.onrender.com";
 export const API = `${BACKEND_URL}/api`;
 
 const api = axios.create({ baseURL: API });
